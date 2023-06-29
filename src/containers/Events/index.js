864952e432aa38d1772bd17f2710c0c830e66954
Scharly OPITZ <scharly.opitz@gmail.com>
2023-06-29 12:52:05 +0200

    ajout keyprops

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -72,6 +72,7 @@ const EventList = () => {
                         {[...Array(pageNumber || 0)].map((_, n) => (
                             // eslint-disable-next-line react/no-array-index-key
                             <a
+                                key={n}
                                 href="#events"
                                 onClick={() => setCurrentPage(n + 1)}
                             >
